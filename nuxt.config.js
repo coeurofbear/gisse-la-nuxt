49502bf0637c3983a80fb40c1194e6f04b4aced6
Gíssela Reyes <coeurofbear@gmail.com>
2022-11-04 17:57:53 +0100

    feat: add css and masonry

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,10 +21,12 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/assets/css/project/all.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/vue-masonry-css', ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
